ae9c2978b0bba24d33373c4b05ea40a1231634a3
unknown <2daw3@HZ114494.H012108.NET>
2022-09-19 13:02:46 +0200

    add actividad 2

---
+++ actividad3/ejer3.php
@@ -0,0 +1,15 @@
+<?php
+    $pares = true;
+    
+    if($pares == true){
+        for ($i = 1; $i < 99; $i++) {
+            echo "<br>".$i++ ;
+        }
+    }
+    else{
+        for ($i = 0; $i < 100; $i++) {
+            echo "<br>".$i++ ;
+        }
+    }
+    ?>
+    
