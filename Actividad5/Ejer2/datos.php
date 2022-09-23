7d435b3d15fe36ba0a90fef6efb09c093ff538e1
root4554 <anass.kbir@hotmail.com>
2022-09-24 01:00:21 +0200

    add ex2, ex3

--- Actividad5/Ejer2/datos.php
+++ Actividad5/Ejer2/datos.php
@@ -7,25 +7,28 @@
     $estado = $_POST['estado'];
     $aficiones = $_POST['aficiones'];
 
+
     // Comprobar que los datos no son vacíos
-    if(empty ($nombre)) echo "El nombre no puede estar vacío <br>";
-    if(empty ($apellidos)) echo "Los apellidos no pueden estar vacíos <br>";
-    if(empty ($edad)) echo "La edad no puede estar vacía <br>";
-    if(empty ($peso)) echo "El peso no puede estar vacío <br>";
-    if(empty ($sexo)) echo "El sexo no puede estar vacío <br>";
-    if(empty ($estado)) echo "El estado no puede estar vacío <br>";
-    if(empty ($aficiones)) echo "Las aficiones no pueden estar vacías <br>";
+    if(empty ($nombre)) echo "<p style='color:red'>El nombre no puede estar vacío</p> ";
+    else echo "<p>Nombre:<b> $nombre</b></p> ";
+    if(empty ($apellidos)) echo "<p style='color:red'> los apellidos no pueden estar vacíos</p> ";
+    else echo "<p>Apellidos:<b> $apellidos</b></p> ";
+    if(empty ($edad)) echo "<p style='color:red'> La edad no puede estar vacía</p> ";
+    else echo "<p>Edad:<b> $edad</b></p> ";
+    if(empty ($peso)) echo "<p style='color:red'> El peso no puede estar vacío</p> ";
+    else echo "<p>Peso:<b> $peso</b></p> ";
+    if(empty ($sexo)) echo "<p style='color:red'> El sexo no puede estar vacío </p> ";
+    else echo "<p>Sexo:<b> $sexo</b></p> ";
+    if(empty ($estado)) echo "<p style='color:red'> El estado no puede estar vacío</p> ";
+    else echo "<p>Estado:<b> $estado</b></p> ";
+    if(empty ($aficiones)) echo "<p style='color:red'>Las aficiones no pueden estar vacías</p> ";
+    else {
+        echo "Aficiones: ";
+        foreach ($aficiones as $aficion) {
+            echo $aficion . ", ";
+        }
+    }
 
-
-    echo "Nombre: " . $nombre . "<br>";
-    echo "Apellidos: " . $apellidos . "<br>";
-    echo "Edad: " . $edad . "<br>";
-    echo "Peso: " . $peso . "<br>";
-    echo "Sexo: " . $sexo . "<br>";
-    echo "Estado: " . $estado . "<br>";
-    //echo "Afeciones: " . $afeciones . "<br>";
-foreach ($aficiones as $aficion) {
-    echo $aficion . ", ";
-}
+echo "<br><br><a href='datos.html' >Volver al formulario</a>";
 
 ?>
